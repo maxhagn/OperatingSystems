8ae4035c1e40b6e30574eb59a4c049365ea988a9
maxhagn <Nepomuk1997>
2021-01-17 21:44:48 +0100

    Some message about the change

--- http/client/client.c
+++ http/client/client.c
@@ -44,40 +44,67 @@ static void usage( void ) {
 static struct Url parse_url( char *url ) {
 
     struct Url parsed_url;
+    char* tmp_host = NULL;
+    char* tmp_path = NULL;
     char ending_chars[] = ";/?:@=&";
 
     if ( strstr( url, "http://" )) {
-        char *url_copy = malloc( strlen( url ) + 1 );
-        strcpy( url_copy, url );
-        url_copy += strlen( "http://" );
+        size_t len = strlen( url );
+        char *url_short = (char *)malloc( len + 1);
+        memcpy( url_short, url, len );
+        url_short[ len ] = '\0';
+        url_short += strlen( "http://" );
 
         char *url_path;
-        url_path = strpbrk( url_copy, ending_chars );
-
+        url_path = strpbrk( url_short, ending_chars );
         if ( url_path == NULL ) {
-            parsed_url.host = malloc( strlen( url_copy ) + 1 );
-            strcpy( parsed_url.host, url_copy );
-            parsed_url.path = malloc( 2 );
-            parsed_url.path = "/\0";
+
+            tmp_host = malloc( strlen( url_short ) + 1 );
+            memset(tmp_host, 0, strlen( url_short ) + 1);
+            memcpy( tmp_host, url_short, strlen( url_short ) );
+
+            tmp_path = malloc( strlen("/") + 1 );
+            memset(tmp_path, 0, 2);
+            char *slash = "/";
+            memcpy( tmp_path, slash, strlen( slash ) );
+
         } else {
 
             int path_length = strlen( url_path );
-            parsed_url.path = malloc( path_length + 1 );
-            parsed_url.path = url_path;
+            tmp_path = malloc(path_length + 1);
+            memset(tmp_path, 0, path_length + 1);
+            memcpy(tmp_path, url_path, path_length);
 
-            int host_length = strlen( url_copy ) - strlen( parsed_url.path );
-            parsed_url.host = malloc( host_length + 1 );
-            strncpy( parsed_url.host, url_copy, host_length );
+            int host_length = strlen( url_short ) - strlen( tmp_path );
+            tmp_host = malloc(host_length + 1);
+            memset(tmp_host, 0, host_length + 1);
+            memcpy(tmp_host, url_short, host_length );
 
-            parsed_url.host[ host_length + 1 ] = '\0';
 
         }
 
-
+        free(url_short - strlen("http://"));
     } else {
+        free( url );
         fprintf( stderr, "The given URL is invalid by this assignment specification" );
         exit( EXIT_FAILURE );
     }
+
+    parsed_url.host = malloc(strlen(tmp_host) + 1);
+    strcpy(parsed_url.host, tmp_host);
+    parsed_url.path = malloc(strlen(tmp_path) + 1);
+    strcpy(parsed_url.path, tmp_path);
+
+
+
+    if ( tmp_path != NULL ) {
+        free(tmp_path);
+    }
+
+    if ( tmp_host != NULL ) {
+        free(tmp_host);
+    }
+
 
     return parsed_url;
 }
@@ -132,6 +159,8 @@ static void request( char *url, char *port, FILE *output_file ) {
         exit( EXIT_FAILURE );
     }
 
+    free(request);
+
     if ( fflush( sockfile ) == EOF ) {
         fprintf( stderr, "Couldn't sent the request to server" );
         exit( EXIT_FAILURE );
@@ -141,9 +170,8 @@ static void request( char *url, char *port, FILE *output_file ) {
     int is_response_body = 0;
     char *line = NULL;
     size_t len = 0;
-    ssize_t read;
 
-    while (( read = getline( &line, &len, sockfile )) != -1 ) {
+    while (( getline( &line, &len, sockfile )) != -1 ) {
         if ( line_counter == 1 ) {
             if ( !strstr( line, "HTTP/1.1" )) {
 
@@ -158,23 +186,38 @@ static void request( char *url, char *port, FILE *output_file ) {
 
                 char *remaining_chars;
                 int status_code = strtol( line_copy, &remaining_chars, 10 );
+                free(line_copy - strlen("HTTP/1.1"));
 
                 if ( status_code != 200 ) {
-                    fprintf( stderr, "Response: %s , No Success\n", line );
+                    free( url );
+                    free(line);
+                    line = NULL;
+                    free(parsed_url.host);
+                    free(parsed_url.path);
+                    freeaddrinfo( ai );
+                    fclose( sockfile );
+                    fprintf( stderr, "Response: %sNo Success\n", line );
                     exit( 3 );
                 }
+
+
+
             }
         } else {
+        if ( is_response_body == 1 ) {
+                        fprintf( output_file, "%s", line );
+                    }
             if ( strcmp( line, "\r\n" ) == 0 ) {
                 is_response_body = 1;
             }
-            if ( is_response_body == 1 ) {
-                fprintf( output_file, "%s", line );
-            }
+
         }
         line_counter++;
     }
 
+    free(line);
+    free(parsed_url.host);
+    free(parsed_url.path);
     freeaddrinfo( ai );
     fclose( sockfile );
 
@@ -232,21 +275,27 @@ int main( int argc, char *argv[] ) {
         usage( );
     }
 
-    char *url = malloc( strlen( argv[ optind ] ));
-    strcpy( url, argv[ optind ] );
+    char *url = malloc( strlen( argv[ optind ] ) + 1);
+    memset( url, 0, strlen( argv[ optind ]) + 1);
+    memcpy( url, argv[ optind ], strlen( argv[ optind ]));
 
-    char *has_chars;
-    int port_numeric = strtol( port, &has_chars, 10 );
-    if ( strlen( has_chars ) > 0 || port_numeric < 0 ) {
+    char *remaining_chars;
+    int port_numeric = strtol( port, &remaining_chars, 10 );
+    if ( strlen( remaining_chars ) > 0 || port_numeric < 0 ) {
         fprintf( stderr, "%s is an invalid port. Ports can't be negative and must be numeric!", port );
+        free( url );
         exit( EXIT_FAILURE );
     }
 
     char *file_path = NULL;
     if ( directory_option != NULL ) {
-        char *url_copy = malloc( strlen( url ));
-        strcpy( url_copy, url );
+        size_t url_length = strlen( url ) + 1;
+        char *url_copy = malloc( url_length );
+        memset(url_copy, 0, url_length);
+        memcpy( url_copy, url, url_length );
+
         char *requested_file_name = strrchr( url_copy, '/' );
+
 
         char *directory_copy = malloc( strlen( directory_option ) + 1 );
         strcpy( directory_copy, directory_option );
@@ -270,9 +319,9 @@ int main( int argc, char *argv[] ) {
             strcat( directory_option, "/index.html" );
         }
 
+        free(url_copy);
         file_path = malloc( strlen( directory_option ) + 1 );
         strcpy( file_path, directory_option );
-        fprintf( stderr, "%s", file_path );
         free( directory_copy );
 
     } else if ( file_option != NULL ) {
@@ -283,11 +332,16 @@ int main( int argc, char *argv[] ) {
     if ( file_path != NULL ) {
         if ( !( output_file = fopen( file_path, "w" ))) {
             fprintf( stderr, "File %s couldn't be accessed. \n", file_path );
+            free( url );
             exit( EXIT_FAILURE );
         }
     }
 
     request( url, port, output_file );
+
+    if ( url != NULL ) {
+        free( url );
+    }
 
     if ( file_path != NULL ) {
         free( file_path );
